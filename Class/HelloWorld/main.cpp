1fb7e5ea4e34906f2c2b11643d72f604f1953bfc
ElijahDeVera <77760455+ElijahDeVera@users.noreply.github.com>
2021-02-22 15:26:20 -0800

    Add files via upload

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Elijah De Vera
+ * Created on January 5, 2021, 12:36 AM
+ * Purpose: First Program "Hello World"
+ */
+
+//System Libraries
+#include <iostream> //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversion, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    cout<<"Hello World"<<endl;
+    
+    // Exit the Program - Cleanup
+    return 0;
+}
+
